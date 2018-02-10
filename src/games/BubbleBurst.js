466879a068130b00d3e69979b99f283d93fb56dd
Anoxic <guanrunjie@gmail.com>
2018-02-10 21:46:18 +0800

    Bubble burst: click to create a bubble

--- src/games/BubbleBurst.js
+++ src/games/BubbleBurst.js
@@ -10,9 +10,9 @@ const BUBBLE_NUMBER = 20;
 const TICK = 40; // milliseconds
 const MOVING_SPEED = 3; // px/tick
 const INITIAL_RADIUS = 5;
-const FINAL_RADIUS = 20;
+const FINAL_RADIUS = 50;
 const GROWING_SPEED = 1.5; // px/tick
-const LIFE_SPAN = 5000 / TICK; // ticks
+const LIFE_SPAN = 5000.0 / TICK; // ticks
 
 const COLORS = ["#FF8A80", "#FF5252", "#FF1744", "#D50000", "#FF80AB", "#FF4081", "#F50057", "#C51162", "#EA80FC", "#E040FB", "#D500F9", "#AA00FF", "#B388FF", "#7C4DFF", "#651FFF", "#6200EA", "#8C9EFF", "#536DFE", "#3D5AFE", "#304FFE", "#82B1FF", "#448AFF", "#2979FF", "#2962FF", "#80D8FF", "#40C4FF", "#00B0FF", "#0091EA", "#84FFFF", "#18FFFF", "#00E5FF", "#00B8D4", "#A7FFEB", "#64FFDA", "#1DE9B6", "#00BFA5", "#B9F6CA", "#69F0AE", "#00E676", "#00C853", "#CCFF90", "#B2FF59", "#76FF03", "#64DD17", "#F4FF81", "#EEFF41", "#C6FF00", "#AEEA00", "#FFFF8D", "#FFFF00", "#FFEA00", "#FFD600", "#FFE57F", "#FFD740", "#FFC400", "#FFAB00", "#FFD180", "#FFAB40", "#FF9100", "#FF6D00", "#FF9E80", "#FF6E40", "#FF3D00", "#DD2C00"];
 
@@ -32,16 +32,17 @@ export default class BubbleBurst extends Component {
     this.handleProblemSolved = this.handleProblemSolved.bind(this);
 
     this.handleResize = this.handleResize.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
   }
 
   handleResize() {
     // If the screen is resized, finish the game immediately because of the
     // level
     this.handleProblemSolved();
-  }
-
-  componentDidUpdate() {
-
   }
 
   startNewProblem() {
@@ -71,18 +72,29 @@ export default class BubbleBurst extends Component {
       bubbles,
     }, () => {
       this.intervalId = setInterval(() => {
-        let bubbles = [...this.state.bubbles].map(b => {
+        let bubbles = [];
+
+        for (let b of this.state.bubbles) {
+          if (b.life) {
+            b.size += GROWING_SPEED;
+            b.size = Math.min(b.size, FINAL_RADIUS);
+
+            if (++b.life > LIFE_SPAN) {
+              continue;
+            }
+          }
+
           let {top, left, horizontalSpeed, verticalSpeed} = b;
 
           let nextTop = top + horizontalSpeed;
           let nextLeft = left + verticalSpeed;
 
           // Check if they are out of bound
-          if (nextTop < b.size || nextTop > HEIGHT) {
+          if (nextTop < 0 || nextTop > HEIGHT) {
             horizontalSpeed = -horizontalSpeed;
           }
 
-          if (nextLeft < b.size || nextLeft > WIDTH) {
+          if (nextLeft < 0 || nextLeft > WIDTH) {
             verticalSpeed = -verticalSpeed;
           }
 
@@ -91,13 +103,40 @@ export default class BubbleBurst extends Component {
           b.horizontalSpeed = horizontalSpeed;
           b.verticalSpeed = verticalSpeed;
 
-          return b;
-        });
+          bubbles.push({...b});
+        }
 
         this.setState({
           bubbles,
         });
       }, TICK);
+    });
+  }
+
+  /**
+   * Handles the click
+   * @param {MouseEvent} e
+   */
+  handleClick(e) {
+    if (this.state.bubbles.length > BUBBLE_NUMBER) {
+      // There is another bubble going on
+      return;
+    }
+
+    let top = e.pageY - e.target.getBoundingClientRect().top;
+    let left = e.pageX - e.target.getBoundingClientRect().left;
+    console.log(top, left);
+
+    this.setState({
+      bubbles: [...this.state.bubbles, {
+        top,
+        left,
+        horizontalSpeed: 0,
+        verticalSpeed  : 0,
+        color          : "#000",
+        size           : INITIAL_RADIUS,
+        life           : 1,
+      }],
     });
   }
 
@@ -123,13 +162,14 @@ export default class BubbleBurst extends Component {
             score={this.state.score}
       >
         <div className="bubble-canvas flex-center"
+             onClick={this.handleClick}
              style={{width: WIDTH, height: HEIGHT}}>
           {this.state.bubbles.map(b =>
             <span className="bubble"
                   key={b.color}
                   style={{
-                    top       : b.top - b.size,
-                    left      : b.left - b.size,
+                    top       : b.top - b.size / 2,
+                    left      : b.left - b.size / 2,
                     background: b.color,
                     width     : b.size,
                     height    : b.size,
